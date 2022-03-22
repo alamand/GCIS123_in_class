435fd755802c357f568032ecf72a359d61ca6c37
alamand <alamand@gmail.com>
2022-03-22 13:32:58 +0400

    comparison of ds properties

---
+++ data_structure_comparisons.py
@@ -0,0 +1,10 @@
+'''
+Data Type   Access  Insert/Delete   Orderable   Unique
+======================================================
+Array       O(c)    O(n)[O(c) end]  Yes         No
+List        O(c)    O(n)[O(c) end]  Yes         No
+Dictionary  O(c)    O(c)            No          Keys
+Set         N/A     O(c)            No          Yes
+Stack       O(c)    O(c)            Is Ordered  No
+Queue       O(c)    O(c)            Is Ordered  No
+'''
