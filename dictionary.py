335548c1d14200ca42fad690df28af753b5a42c4
alamand <alamand@gmail.com>
2022-03-22 13:32:04 +0400

    Added to dictionary code

--- dictionary.py
+++ dictionary.py
@@ -8,14 +8,36 @@ def change_dictionary(dict):
     dict["GPA"] = 4.0
     dict["age"] = 20
     dict["age"] = 26    #replaces age
+    print(dict.get("age"))
+    # dict.clear()
 
 def print_dictionary(dict):
     print("Length: ", len(dict))
+    
+    # print keys and values
     for key in dict:
         print(key, ":", dict[key], end=" ")
         print()
-    # if "GPA" in dict:
-    #     print(dict["GPA"])
+
+    # print keys only
+    list = dict.keys()
+    print("Keys: ", list)
+
+    # print values only
+    list = dict.values()
+    print("Values: ", list)
+
+    # make a copy
+    dict_copy = dict.copy()
+    print("copy: ", dict_copy)
+
+    # remove an items using the key to identify it
+    removed = dict.pop("age")
+    print("age removed: ", dict)
+
+    # check if the key exists before printing it
+    if "age" in dict:
+        print(dict["age"])
 
 def main():
     d1, d2, d3 = create_dictionaries()
@@ -23,6 +45,6 @@ def main():
     print(d3)
     # print(d3["credits"])      # KeyError! Use an if statement to check that the key exists
     print_dictionary(d3) # ordered but not sorted
-    # print(sorted(d3))
+    # print(sorted(d3))  # only sorts the keys
 
 main()
